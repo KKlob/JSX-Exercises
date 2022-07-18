4bf2e7e33ed37aec4b56bec1442eb1366b54ba65
kainklob <klobk23@gmail.com>
2022-07-18 18:27:31 -0400

    finished part 2

---
+++ part-2/app.js
@@ -0,0 +1,17 @@
+const App = () => {
+    return (
+        <div>
+            <p>
+                <Tweet username="username1" name="user1" date="7/18/2022" message="This is the first message" />
+            </p>
+            <hr />
+            <p>
+                <Tweet username="username2" name="user2" date="7/17/2022" message="This is the second message" />
+            </p>
+            <hr />
+            <p>
+                <Tweet username="username3" name="user3" date="7/16/2022" message="This is the third message" />
+            </p>
+        </div>
+    )
+}
